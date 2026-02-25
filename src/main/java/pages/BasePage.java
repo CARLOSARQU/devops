fed9f6219a5f3b7e02787521c853dbe5f8e53ab1
CARLOSARQU <carlosmidwar6@gmail.com>
2026-02-25 18:30:39 -0500

    lib add

--- src/main/java/pages/BasePage.java
+++ src/main/java/pages/BasePage.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public abstract class BasePage {
     protected AndroidDriver driver;
